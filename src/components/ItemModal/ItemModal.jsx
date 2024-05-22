52ba98624c3305af9386f9cc1f606bb84695e2f1
ImaniW96.git <imaniwalker0305@gmail.com>
2024-05-22 16:39:09 -0400

    Update

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -3,9 +3,11 @@ function ItemModal({ activeModal, onClick, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"} `}>
       <div className="modal__content modal__content_type_image">
-        <button onClick={onClick} type="button" className="modal__close">
-          Close
-        </button>
+        <button
+          onClick={onClick}
+          type="button"
+          className="modal__close"
+        ></button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
