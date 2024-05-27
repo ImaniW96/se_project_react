33a16f15a3f8549baf56167e6fe673f2edd4978b
ImaniW96.git <imaniwalker0305@gmail.com>
2024-05-27 15:33:19 -0400

    Toggle switch update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -10,6 +11,7 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt={logo} />
+      <ToggleSwitch />
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
       </p>
