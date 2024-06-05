829328035174d6bc41ee22dfe659f8a7962e5df4
ImaniW96.git <imaniwalker0305@gmail.com>
2024-06-05 16:20:24 -0400

    Update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,28 @@
 const baseUrl = "http://localhost:3001";
+const headers = "Content-Type: application/json";
+header = this._headers;
 function getItems() {
   return fetch(`${baseUrl}/items`).then((res) => {
     return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
 }
+
 export { getItems };
 
-function deleteCard() {
-  return fetch(`${baseUrl}/delete`).then((res) => {});
-}
+const deleteCardApi = (Id) => {
+  return fetch(`${this._baseUrl}/id/${Id}`, {
+    method: "DELETE",
+    headers: this._headers,
+  }).then((res) => {
+    return this.checkResponse(res);
+  });
+};
+const addItemsAPI = ({ name, weather, imageUrl }) => {
+  return fetch(`${this._baseUrl}/id/${Id}`, {
+    method: "POST",
+    headers: this._headers,
+    body: JSON.stringify({ name, weather, imageUrl }),
+  }).then((res) => {
+    return this.checkResponse(res);
+  });
+};
