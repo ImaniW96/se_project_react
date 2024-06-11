474c806518f54a960b25682a18a474cd1c77a40f
ImaniW96.git <imaniwalker0305@gmail.com>
2024-06-10 21:30:12 -0400

    Add item update

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -2,12 +2,18 @@ import ItemCard from "../ItemCard/ItemCard";
 import "./ClothesSection.css";
 
 // import { defaultClothingItems } from "../../utils/constants";
-function ClothesSection({ handleCardClick, clothingItems }) {
+function ClothesSection({ handleCardClick, clothingItems, handleAddClick }) {
   return (
     <div className="clothes-section">
       <div className="clothes__button">
         <p className="clothes-section_your-itemsText">Your items</p>
-        <button className="clothing-section_addBtn">+ Add New</button>
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="clothing-section_addBtn"
+        >
+          + Add New
+        </button>
       </div>
       <ul className="clothes-section__items">
         {clothingItems.map((item) => {
